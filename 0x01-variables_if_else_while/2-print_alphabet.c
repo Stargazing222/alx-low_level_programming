dd5cb7c54e690d78bcf77d182c4b0f73ae04feea
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-09 17:16:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void){
+
+	char lower = 'a';
+
+	while (lower <= 'z'){
+		
+		putchar(lower);
+		lower++;
+	}
+	putchar('\n');
+	return (0);
+}
