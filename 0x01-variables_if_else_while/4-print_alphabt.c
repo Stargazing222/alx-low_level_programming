7dfdc6ff409259bbd39338dbc4b4f1b19d9dd004
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-12 03:19:45 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,9 +6,16 @@
 * tjis program prints "Programming is postitve, zero, or negative
 * Return 0
 */
-int main(0)
+int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-<<<
+if (ch = 'a ' ; ch <=  'z' ; ch++)
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
