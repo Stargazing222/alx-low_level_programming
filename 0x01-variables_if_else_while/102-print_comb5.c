c2dc67e201c16a7d6cde66d5d5b8fa6eca566a4a
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-12 09:52:35 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,32 +14,25 @@ int main(void)
 	int p, q;
 
 	for (p = 0; p <= 98; p++)
-		{
-	for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-																					putchar(' ');
-			putchar((q / 10) + '0');
+	{
+		for (q = p + 1; q <= 99; q++)
+			{
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
+				putchar(' ');
+				putchar((q / 10) + '0');
 				putchar((q % 10) + '0');
+
 				if (p == 98 && q == 99)
+					continue;
 
-																																			continue
+				putchar(',');
+				putchar(' ');
+			}
+	}
 
-																																	putchar(',');
+	putchar('\n');
 
-																																				putchar(' ');
-
-																																						}
-
-									}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
+
