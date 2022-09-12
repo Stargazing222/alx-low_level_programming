f8fee607e3c4da6d065e8832e99065fe415dffa2
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-12 02:57:07 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void){
-
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
+for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
-	putchar('\n');
-
-	return (0);
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- *
- *	main - Entry point
- *
- *	Return: Always 0 (Success)
- */
-
-int main(void){
-
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
-
-	return (0);
-
-}
+*
+* main -print if number is positive, zero, or negative
+* Description : using the main function
+* tjis program prints "Programming is postitve, zero, or negative
+* Return 0
+*/
+int main(0)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+<<<
