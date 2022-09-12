b81c13796a468cb821388a96f8983421a8ad1585
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-12 09:39:31 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,70 +8,38 @@
 
 /**
  *
- *  * main - Entry point
+ *   main - Entry point
+ *   
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */   
 
 int main(void)
+{	
+	int d, p, q;
 
-{
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			for (q =p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
 
-		int d, p, q;
+					if (d == '7' && p == '8')
+						continue;
 
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-
-			for (d = '0'; d < '9'; d++)
-
-					{
-
-								for (p = d + 1; p <= '9'; p++)
-
-											{
-
-															for (q = p + 1; q <= '9'; q++)
-
-																			{
-
-																								if ((p != d) != q)
-
-																													{
-
-																																			putchar(d);
-
-																																								putchar(p);
-
-																																													putchar(q);
-
-
-
-																																																		if (d == '7' && p == '8')
-
-																																																									continue;
-
-
-
-																																																							putchar(',');
-
-																																																												putchar(' ');
-
-																																																																}
-
-																											}
-
-																	}
-
-									}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
