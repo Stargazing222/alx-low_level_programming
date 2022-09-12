47aba087155daaa696e8d57c75353cf23d5c3e4d
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-12 08:47:44 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,17 +12,17 @@ int main(void)
 		int d;
 
 		for (d = '0'; d <= '9'; d++)
-		{	
+		{
 			putchar(d);
 			if (d != '9')
 			{
 					putchar(',');
 					putchar(' ');
-		
+
 			}
-	
+
 		}
-		putchar('\n');	
-	
+		putchar('\n');
+
 		return (0);
 }
