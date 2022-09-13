30b0e747cef7848dfbc19a6f01e2155064e710f2
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-13 15:06:04 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,21 @@
+#include  <unistd.h>
 #include "main.h"
 
 /**
- * print alphabet - Check description
- * Description: It prints the alphabet in lowercase
- * Return: Nothing.
+ * print_alphabet - Entry point
+ * Return:0
  */
-void print_alphabet (void)
-{
-	char i;
 
-	for (i= 'a': i <= 'z': i++)
-		_putchar(i);
-	_putchar ( \n°) ;
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Description: contains all functions prototype used in this project*/
 
-
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +18,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif
+#endif /* MAIN_H */
