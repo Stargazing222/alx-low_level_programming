a1c0ca38a7d30c35ba9915e8417829f9db8ab694
Stargazing222 <refilwenaledi8@gmail.com>
2022-09-29 03:26:45 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include  "main.h"
+
+/**
+ * _length - calculates the length
+ * @s: string
+ * Return: length of string
+ */
+int _length(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + _length(s));
+	}
+	return (0);
+}
+
+/**
+ * _check - checks the string
+ * @str: string
+ * @n: num
+ * @i: iterator
+ * Return: 0 or 1
+ */
+int _check(char *str, int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (str[i] == str[n])
+		return (_check(str, n - 1, i + 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - shows if string is palindrome or not
+ * @s: string
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	int n = _length(s);
+	int i = 0;
+
+	return (_check(s, n - 1, i));
+}
